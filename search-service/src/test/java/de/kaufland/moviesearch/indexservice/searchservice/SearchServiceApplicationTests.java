1ebf11f4053aecba1d8911c6d83ff1e032867da4
Ali <ali.zare71@gmail.com>
2022-09-08 13:57:29 +0430

    search-service: Search service init.

---
+++ search-service/src/test/java/de/kaufland/moviesearch/indexservice/searchservice/SearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kaufland.moviesearch.indexservice.searchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
