88ce13d0ba39e7fbffd8b74eb59f39cdb310249e
Ali <ali.zare71@gmail.com>
2022-09-08 10:51:40 +0430

    ingestion api service init.

---
+++ ingestion-api-service/src/main/java/de/kaufland/moviesearch/ingestionapiservice/IngestionApiServiceApplication.java
@@ -0,0 +1,13 @@
+package de.kaufland.moviesearch.ingestionapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestionApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngestionApiServiceApplication.class, args);
+    }
+
+}
---
+++ ingestion-api-service/src/test/java/de/kaufland/moviesearch/ingestionapiservice/IngestionApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kaufland.moviesearch.ingestionapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngestionApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
