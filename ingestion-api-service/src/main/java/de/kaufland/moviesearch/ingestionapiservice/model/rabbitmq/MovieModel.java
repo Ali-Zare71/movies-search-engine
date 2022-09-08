8ac606735ee54faa7466b30827194e67af184216
Ali <ali.zare71@gmail.com>
2022-09-08 11:55:43 +0430

    ingestion-api-service: rabbitmq added.

---
+++ ingestion-api-service/src/main/java/de/kaufland/moviesearch/ingestionapiservice/model/rabbitmq/MovieModel.java
@@ -0,0 +1,24 @@
+package de.kaufland.moviesearch.ingestionapiservice.model.rabbitmq;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class MovieModel implements Serializable {
+    private Long id;
+    private String title;
+    private String year;
+    private String runtime;
+    private List<String> genres;
+    private String director;
+    private String actors;
+    private String plot;
+    private String posterUrl;
+
+}
---
+++ ingestion-api-service/src/main/java/de/kaufland/moviesearch/ingestionapiservice/service/RabbitMqProducerService.java
@@ -0,0 +1,35 @@
+package de.kaufland.moviesearch.ingestionapiservice.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.kaufland.moviesearch.ingestionapiservice.model.rabbitmq.MovieModel;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMqProducerService {
+    private static final Logger infoLog = LogManager.getLogger("infoLog");
+    private static final Logger LOGGER = LogManager.getLogger(RabbitMqProducerService.class.getName());
+    private final RabbitTemplate template;
+    private final Queue queue;
+    private final ObjectMapper objectMapper;
+
+
+    public RabbitMqProducerService(RabbitTemplate template, Queue queue, ObjectMapper objectMapper) {
+        this.template = template;
+        this.queue = queue;
+        this.objectMapper = objectMapper;
+    }
+
+    public void send(MovieModel movieModel) {
+        try {
+            String message = objectMapper.writeValueAsString(movieModel);
+            this.template.convertAndSend(queue.getName(), message);
+        } catch (Exception e) {
+            LOGGER.catching(e);
+        }
+
+    }
+}
